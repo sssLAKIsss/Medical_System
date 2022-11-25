90033bad086cfc5d7487ae06db8d6ecde30c3730
sssLAKIsss <slakis210195@gnail.com>
2022-11-26 01:42:56 +0500

    + fix version rules

--- person/person-service/src/main/java/ru/vtb/config/OpenApiConfig.java
+++ person/person-service/src/main/java/ru/vtb/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,14 +14,19 @@ public class OpenApiConfig implements OpenApiCustomiser {
     private static final String API_LICENSE = "Лицензировано мной";
     private static final String CONTACT_NAME = "Я";
     private static final String CONTACT_EMAIL = "я@я.я";
-    private static final String TITLE = "Справочник клиентов";
-    private static final String DESCRIPTION = "Сервис предназначен для чего-то очень важного";
+    private static final String DESCRIPTION = "Справочник клиентов";
     private static final String DOCS_URL = "когда-нибудь появится";
+
+    @Value("${app.version}")
+    private String version;
+    @Value("${app.name}")
+    private String appName;
 
     @Override
     public void customise(OpenAPI openApi) {
         openApi.info(new Info()
-                .title(TITLE)
+                .title(appName)
+                .version(version)
                 .description(DESCRIPTION)
                 .license(new License().name(API_LICENSE))
                 .contact(new Contact().name(CONTACT_NAME).email(CONTACT_EMAIL)));
